d845c4c3feed0f12a38f035c9e68c5f7133b58a8
ivan <qwIvan@users.noreply.github.com>
2017-11-28 01:58:33 +0800

    support for python3

--- train.py
+++ train.py
@@ -69,7 +69,7 @@ def read_data(img_glob):
 
 
 def unzip(b):
-    xs, ys = zip(*b)
+    xs, ys = list(zip(*b))
     xs = numpy.array(xs)
     ys = numpy.array(ys)
     return xs, ys
@@ -202,11 +202,11 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                                               r[3] < 0.5)))
         r_short = (r[0][:190], r[1][:190], r[2][:190], r[3][:190])
         for b, c, pb, pc in zip(*r_short):
-            print "{} {} <-> {} {}".format(vec_to_plate(c), pc,
-                                           vec_to_plate(b), float(pb))
+            print("{} {} <-> {} {}".format(vec_to_plate(c), pc,
+                                           vec_to_plate(b), float(pb)))
         num_p_correct = numpy.sum(r[2] == r[3])
 
-        print ("B{:3d} {:2.02f}% {:02.02f}% loss: {} "
+        print(("B{:3d} {:2.02f}% {:02.02f}% loss: {} "
                "(digits: {}, presence: {}) |{}|").format(
             batch_idx,
             100. * num_correct / (len(r[0])),
@@ -215,7 +215,7 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
             r[4],
             r[5],
             "".join("X "[numpy.array_equal(b, c) or (not pb and not pc)]
-                                           for b, c, pb, pc in zip(*r_short)))
+                                           for b, c, pb, pc in zip(*r_short))))
 
     def do_batch():
         sess.run(train_step,
@@ -240,9 +240,9 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                 if batch_idx % report_steps == 0:
                     batch_time = time.time()
                     if last_batch_idx != batch_idx:
-                        print "time for 60 batches {}".format(
+                        print("time for 60 batches {}".format(
                             60 * (last_batch_time - batch_time) /
-                                            (last_batch_idx - batch_idx))
+                                            (last_batch_idx - batch_idx)))
                         last_batch_idx = batch_idx
                         last_batch_time = batch_time
 
